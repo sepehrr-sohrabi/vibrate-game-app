5101b1264b925294da913ad03e348b8f38cd5f76
sepehr-sohrabi <165030560+sepehrr-sohrabi@users.noreply.github.com>
2024-05-14 22:25:27 +0330

    Add files via upload
    dokisepi

---
+++ vibrate-your-phone-main/components/App.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import { View, Vibration, StyleSheet, Platform } from "react-native";
+import PowerButton from "./powerButton";
+import MainImage from "./mainImage";
+
+const App = () => {
+  const [isVibratorOn, setIsVibratorOn] = useState(false);
+
+  useEffect(() => {
+    isVibratorOn
+      ? Vibration.vibrate(Platform.OS === "android" ? [0, 10000] : [1000], true)
+      : Vibration.cancel();
+  }, [isVibratorOn]);
+
+  return (
+    <View style={styles.container}>
+      <MainImage isVibratorOn={isVibratorOn} />
+      <PowerButton
+        onPress={() => {
+          setIsVibratorOn(!isVibratorOn);
+        }}
+        isVibratorOn={isVibratorOn}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    gap: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#000",
+  },
+});
+
+export default App;
